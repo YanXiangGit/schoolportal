1ce1e4a5d2faa6874d558df1b5295a602d9e49ec
yanxianggit <285318758@qq.com>
2018-12-03 15:43:41 +0800

    111

---
+++ src/main/java/com/portal/model/BaseJsgn.java
@@ -0,0 +1,37 @@
+package com.portal.model;
+
+
+public class BaseJsgn implements java.io.Serializable{
+
+  private String jsbh;
+  private String gnxbh;
+  private java.util.Date gxsj;
+
+
+  public String getJsbh() {
+    return jsbh;
+  }
+
+  public void setJsbh(String jsbh) {
+    this.jsbh = jsbh;
+  }
+
+
+  public String getGnxbh() {
+    return gnxbh;
+  }
+
+  public void setGnxbh(String gnxbh) {
+    this.gnxbh = gnxbh;
+  }
+
+
+  public java.util.Date getGxsj() {
+    return gxsj;
+  }
+
+  public void setGxsj(java.util.Date gxsj) {
+    this.gxsj = gxsj;
+  }
+
+}
